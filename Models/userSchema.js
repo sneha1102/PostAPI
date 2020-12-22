e6c5f2737346f392c10e0351800e0048ef2f80fc
sneha suman <snehasumantezu@gmail.com>
2020-12-22 10:57:36 +0530

    change

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema({
   id: Number,
   username: String,
-  // senderMsgDetails:[{type:Object,default:{}}],
-  // receiverMsgDetails:[{type:Object,default:{}}],
-
   msgDetails: [{ type: Object, default: {} }],
 });
 const User = mongoose.model("User", usersSchema);
