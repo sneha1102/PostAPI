d4e8026a9141a723725bd9d56327dbdb6bc35307
sneha suman <snehasumantezu@gmail.com>
2020-12-23 12:32:27 +0530

    changes

--- route/userRoute/index.js
+++ route/userRoute/index.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../../model/userSchema");
 const Message = require("../../model/messageSchema");
+
 //to create new user
 exports.addNewUser = function (req, res) {
   const user = new User(req.body);
